03f4343375ee7a3172d97bbd55fbe9941127056d
liangyuanzu <liangyuanzu@gmail.com>
2022-01-14 00:45:10 +0800

    test: rm markdown test

--- webapp/cypress/integration/basic.spec.ts
+++ webapp/cypress/integration/basic.spec.ts
@@ -24,14 +24,4 @@ context('Basic', () => {
       .url()
       .should('eq', 'http://localhost:3333/')
   })
-
-  it('markdown', () => {
-    cy.get('[title="About"]')
-      .click()
-      .url()
-      .should('eq', 'http://localhost:3333/about')
-
-    cy.get('pre.language-js')
-      .should('exist')
-  })
 })
